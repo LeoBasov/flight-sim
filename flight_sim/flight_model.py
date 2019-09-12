7754a6fe7257020ef68459d3e421d3897a8e3899
Leo Basov <basov.leo@gmail.com>
2019-09-12 11:14:28 +0200

    Extended flight model

--- flight_sim/flight_model.py
+++ flight_sim/flight_model.py
@@ -3,9 +3,14 @@ The flight model is a 2D model for the flight profile.
 
 """
 
+from .atmosphere import Atmosphere
+
 class FlightModel:
     def __init__(self):
-        pass
+        self.angle_of_attack = 0.0
+        self.height = 0.0
+
+        self.atmospehere = Atmosphere()
 
     def thrust(self):
         return (0, 0)
@@ -13,5 +18,8 @@ class FlightModel:
     def drag(self):
         return (0, 0)
 
-    def weight(self):
+    def weight_force(self):
         return (0, 0)
+
+    def lift(self):
+        pass
