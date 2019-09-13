e865792ed00ffb6841eadfda34581f21486c1607
leo <basov.leo@googlemail.com>
2019-09-13 08:56:08 +0200

    Extended plane model

--- flight_sim/models/plane.py
+++ flight_sim/models/plane.py
@@ -5,7 +5,10 @@ This module contains classes the represent the current kinetic state of the syst
 
 class Plane:
     def __init__(self):
-        pass
+        self.aero_model = None
+        self.engine_model = None
+        self.flight_model = None
+        self.state = State()
 
 class State:
     def __init__(self):
