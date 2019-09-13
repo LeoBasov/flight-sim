b50b8b14b08d34fa2191dfc3322d6c50b3cdc094
leo <basov.leo@googlemail.com>
2019-09-13 09:17:39 +0200

    Modified plane

--- flight_sim/models/plane.py
+++ flight_sim/models/plane.py
@@ -8,8 +8,4 @@ class Plane:
         self.aero_model = None
         self.engine_model = None
         self.flight_model = None
-        self.state = State()
-
-class State:
-    def __init__(self):
-        pass
+        self.kinetic_state = None
