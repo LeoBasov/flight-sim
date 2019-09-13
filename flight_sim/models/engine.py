10a09261b3f642ed2ec536895e057ee4e8846b1a
leo <basov.leo@googlemail.com>
2019-09-13 10:27:15 +0200

    Implemented engine and test

--- flight_sim/models/engine.py
+++ flight_sim/models/engine.py
@@ -2,3 +2,29 @@
 Describes the reaction of engine properties to the current state
 
 """
+
+from .atmosphere import Atmosphere
+
+class Engine:
+    def __init__(self):
+        pass
+
+    def thrust(self, height, velocity):
+        return 0.0
+
+class JP5_H2O2(Engine):
+    def __init__(self, mass_flow):
+        super().__init__()
+        self.ce_ground = 3168.63
+        self.ce_vacuum = 3286.35
+        self.mass_flow = mass_flow
+        self.atmosphere = Atmosphere()
+
+    def get_ce(self, position):
+        rho = self.atmosphere.density(position[1])
+        frac = rho/self.atmosphere.rho_0
+
+        return self.ce_vacuum*(1.0 - frac) + self.ce_ground*frac
+
+    def thrust(self, position, velocity):
+        return self.get_ce(position)*self.mass_flow
--- tests/unit/models/test_engine.py
+++ tests/unit/models/test_engine.py
@@ -3,8 +3,32 @@ import sys
 
 sys.path.append('../../.')
 
-#from thermal import domain
+import flight_sim.models.engine as eng
 
-class EngineTest(unittest.TestCase):
-	def test_init(self):
-		self.assertTrue(False)
+class JP5_H2O2_Test(unittest.TestCase):
+	def test_get_ce(self):
+		mass_flow = 10
+		height1 = 0
+		height2 = 100000
+		engine = eng.JP5_H2O2(mass_flow)
+
+		ce_ground = engine.get_ce((0, height1))
+		ce_vacuum = engine.get_ce((0, height2))
+
+		self.assertAlmostEqual(ce_ground, engine.ce_ground, 3)
+		self.assertAlmostEqual(ce_vacuum, engine.ce_vacuum, 3)
+
+	def test_thrust(self):
+		mass_flow = 10
+		height1 = 0
+		height2 = 100000
+		engine = eng.JP5_H2O2(mass_flow)
+
+		ce_ground = engine.get_ce((0, height1))
+		ce_vacuum = engine.get_ce((0, height2))
+
+		thrust_ground = engine.thrust((0, height1), (0, 0))
+		thrust_vacuum = engine.thrust((0, height2), (0, 0))
+
+		self.assertAlmostEqual(ce_ground*mass_flow, thrust_ground, 3)
+		self.assertAlmostEqual(ce_vacuum*mass_flow, thrust_vacuum, 3)
