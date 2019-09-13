a2d84f1d351dd54a31cf5ebb0c45c0274234489a
leo <basov.leo@googlemail.com>
2019-09-13 10:31:28 +0200

    Added solver module

---
+++ flight_sim/solver.py
@@ -0,0 +1,27 @@
+class Aero:
+    def __init__(self):
+        pass
+
+    def execute(self, plane):
+        pass
+
+class Flight:
+    def __init__(self):
+        pass
+
+    def execute(self, plane):
+        pass
+
+class Kinetic:
+    def __init__(self):
+        pass
+
+    def execute(self, plane):
+        pass
+
+class Flight:
+    def __init__(self):
+        pass
+
+    def execute(self, plane):
+        pass
