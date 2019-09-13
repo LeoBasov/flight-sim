eaa572dfbfae713304c7ee53b0a6ba0db10803d9
leo <basov.leo@googlemail.com>
2019-09-13 10:14:50 +0200

    Added tests

---
+++ tests/unit/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from models.test_engine import*
+
+if __name__ == '__main__':
+	unittest.main()
---
+++ tests/unit/models/test_engine.py
@@ -0,0 +1,10 @@
+import unittest
+import sys
+
+sys.path.append('../../.')
+
+#from thermal import domain
+
+class EngineTest(unittest.TestCase):
+	def test_init(self):
+		self.assertTrue(False)
