8d8ecf9b05adad6753e18bd9d0fa1494cddb0597
Leo Basov <basov.leo@gmail.com>
2019-09-14 09:48:27 +0200

    Added drag calculation

--- flight_sim/models/aero.py
+++ flight_sim/models/aero.py
@@ -7,15 +7,21 @@ class Aero:
     def __init__(self):
         pass
 
-    def get_lift_coefficient(self, kinetic_state):
+    def lift_coefficient(self, kinetic_state):
         return 0.0
 
-    def get_drag_coefficient(self, kinetic_state):
+    def drag_coefficient(self, kinetic_state):
+        return 0.0
+
+    def ref_area(self, kinetic_state):
         return 0.0
 
 class Simple(Aero):
     def __init__(self):
         super().__init__()
 
-    def get_drag_coefficient(self, kinetic_state):
+    def drag_coefficient(self, kinetic_state):
         return 0.05
+
+    def ref_area(self, kinetic_state):
+        return 30.0
--- flight_sim/solver.py
+++ flight_sim/solver.py
@@ -3,14 +3,34 @@ Solver module.
 
 """
 
+import math
 import numpy as np
+from .models.atmosphere import Atmosphere
 
 class Aero:
     def __init__(self):
-        pass
+        self.atmosphere = Atmosphere()
 
     def execute(self, plane, dt):
-        pass
+        ref_area = plane.aero_model.ref_area(plane.kinetic_state)
+        velocity = np.linalg.norm(plane.kinetic_state.velocity)
+        rho = self.atmosphere.density(plane.kinetic_state.position[1])
+        lift = np.zeros(2)
+        drag = np.zeros(2)
+        c_l = plane.aero_model.lift_coefficient(plane.kinetic_state)
+        c_d = plane.aero_model.drag_coefficient(plane.kinetic_state)
+
+        if velocity > 0:
+            flight_vec = plane.kinetic_state.velocity/velocity
+        else:
+            flight_vec = np.zeros(2)
+
+        lift[0] = 0.5*c_l*rho*velocity*velocity*ref_area*math.cos(plane.kinetic_state.angle)
+        lift[1] = 0.5*c_l*rho*velocity*velocity*ref_area*math.sin(plane.kinetic_state.angle)
+
+        drag = -1.0*0.5*c_d*rho*velocity*velocity*ref_area*flight_vec
+
+        plane.kinetic_state.total_force += lift + drag
 
 class Flight:
     def __init__(self):
