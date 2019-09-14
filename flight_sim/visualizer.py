fb275ed8dc22b3778f595c48624091c0d6253957
Leo Basov <basov.leo@gmail.com>
2019-09-14 09:10:30 +0200

    Modified visualizer

--- flight_sim/visualizer.py
+++ flight_sim/visualizer.py
@@ -8,9 +8,14 @@ class Visualizer:
     def plot_height(self, flight_path, dt, itterations):
         time = np.linspace(0, dt*itterations, itterations + 1)
         height = []
+        space = 100000*np.ones(len(flight_path))
 
         for position in flight_path:
             height.append(position[1])
 
-        plt.plot(time, height)
+        plt.plot(time, height, label = 'Flight height')
+        plt.plot(time, space, label = 'Space', linestyle =  '--')
+        plt.legend()
+        plt.xlabel('Flight time [s]')
+        plt.ylabel('Altitude [m]')
         plt.show()
