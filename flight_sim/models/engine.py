5cc4f37858b948c9e04d43a64817fd754cfad681
Leo Basov <basov.leo@gmail.com>
2019-09-14 10:16:39 +0200

    Added HTPB_N2O engine

--- flight_sim/models/engine.py
+++ flight_sim/models/engine.py
@@ -28,3 +28,20 @@ class JP5_H2O2(Engine):
 
     def thrust(self, position, velocity):
         return self.get_ce(position)*self.mass_flow
+
+class HTPB_N2O(Engine):
+    def __init__(self, mass_flow):
+        super().__init__()
+        self.ce_ground = 2423.07 #3168.63 whute paper
+        self.ce_vacuum = 2519.99 #3286.35
+        self.mass_flow = mass_flow
+        self.atmosphere = Atmosphere()
+
+    def get_ce(self, position):
+        rho = self.atmosphere.density(position[1])
+        frac = rho/self.atmosphere.rho_0
+
+        return self.ce_vacuum*(1.0 - frac) + self.ce_ground*frac
+
+    def thrust(self, position, velocity):
+        return self.get_ce(position)*self.mass_flow
