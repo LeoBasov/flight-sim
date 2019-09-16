dd7fec2139445b3aa7e5469bae062a092c059ebb
leo <basov.leo@googlemail.com>
2019-09-16 10:11:29 +0200

    Extended writer

--- flight_sim/writer.py
+++ flight_sim/writer.py
@@ -1,11 +1,12 @@
 import time
 import os
+import csv
 
 class Writer:
-    def __init__(self):
-        self.file_dir = './output/' + time.strftime('%Y%m%d_%H%M%S', time.localtime())
+	def __init__(self):
+		self.file_dir = './output/' + time.strftime('%Y%m%d_%H%M%S', time.localtime())
 
-    def write_to_file(self, step):
+	def write_to_file(self, kinetic_state, time):
 		try:
 			os.makedirs(self.file_dir)
 
@@ -13,7 +14,12 @@ class Writer:
 			pass
 
 		finally:
-			self.write_flight_path(self.file_dir)
+			self.write_kinetic_state(self.file_dir, kinetic_state, time)
 
-    def write_flight_path(self):
-        pass
+	def write_kinetic_state(self, file_dir, kinetic_state, time):
+		file_name = file_dir + '/kinetic_state.csv'
+		row = [time, kinetic_state.position[0], kinetic_state.position[1], kinetic_state.velocity[0], kinetic_state.velocity[1]]
+
+		with open(file_name, 'a') as csvfile:
+			writer = csv.writer(csvfile)
+			writer.writerow(row)
