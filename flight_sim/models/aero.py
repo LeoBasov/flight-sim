c0f061d7758fe6499963ab9d7f682c8072ff9f10
leo <basov.leo@googlemail.com>
2019-09-16 09:59:57 +0200

    Modifed aero

--- flight_sim/models/aero.py
+++ flight_sim/models/aero.py
@@ -21,7 +21,7 @@ class Simple(Aero):
         super().__init__()
 
     def drag_coefficient(self, kinetic_state):
-        return 0.05
+        return 0.15
 
     def ref_area(self, kinetic_state):
-        return 30.0
+        return 10.0
