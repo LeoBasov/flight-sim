1b9391adebc59a7d899791d08ff754c6c077eed8
leo <basov.leo@googlemail.com>
2019-10-28 15:56:08 +0100

    Added basoc pilot.

--- flight_sim/models/flight.py
+++ flight_sim/models/flight.py
@@ -2,3 +2,10 @@
 Describes the reaction of a virtual pilot to the current state
 
 """
+
+class Pilot:
+    def __init__(self):
+        pass
+
+    def fly(self, plane, dt):
+        plane.fire_engine(dt)
