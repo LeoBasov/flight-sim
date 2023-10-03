ff1d41dee64561a267bf285601b9d2efa62e24e7
Leo Basov <leo.basov@dlr.de>
2023-10-02 20:59:57 -0600

    updated python files

---
+++ S2/python/kinetic_dimless.py
@@ -0,0 +1,79 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+from scipy import constants as cst
+
+from atmosphere import Atmosphere
+
+atm = Atmosphere()
+g = cst.g
+c0 = atm.speed_of_sound(0.0)
+
+rho_ref = atm.density(0.0)
+t_ref = c0/g
+ddot_x_ref = g
+dot_x_ref = ddot_x_ref*t_ref
+x_ref = dot_x_ref*t_ref
+
+class Parameters:
+    def __init__(self):
+        self.T = 1.0
+        self.Ma_ce = 2.0
+        self.Ma_L = 0.3
+        self.CD = 0.1
+        self.CA_L = 0.1
+        self.mf = 0.5
+        
+        self.set_up()
+        
+    def set_up(self):
+        self.t_meco = self.mf * self.Ma_ce / self.T
+        
+def _calc_rho(t, y, params):
+    H = y[0] * x_ref
+    return atm.density(H) / rho_ref
+
+def _calc_CD(t, y, params):
+    return params.CD
+
+def _calc_T_by_W(t, y, params):
+    if t <= params.t_meco:
+        return params.T / (1.0 - t * params.T / params.Ma_ce)
+    else:
+        return 0.0
+
+def _calc_M(t, y, params):
+    numerator = 1.0 - params.mf
+    denumerator = 1.0 - t * params.T / params.Ma_ce
+    
+    return min(numerator/denumerator, 1.0)
+
+def _func(t, y, params):
+    T_by_W = _calc_T_by_W(t, y, params)
+    rho = _calc_rho(t, y, params)
+    CD = _calc_CD(t, y, params)
+    M = _calc_M(t, y, params)
+    
+    Ma2 = y[1]**2 / params.Ma_L**2
+    C = CD/params.CA_L
+    
+    ddot_x = T_by_W - rho * Ma2 * C * M - 1.0
+    
+    return (y[1], ddot_x)
+
+def _turn_around(t, y, params):
+    return y[1]
+
+_turn_around.terminal = True
+_turn_around.direction = -1
+
+def solve_trajectory(t, y0, params):
+    params.set_up()
+    
+    params.c0 = atm.speed_of_sound(0.0)
+    params.rho_ref = atm.density(0.0)
+    params.t_ref = c0/g
+    params.ddot_x_ref = g
+    params.dot_x_ref = ddot_x_ref*t_ref
+    params.x_ref = dot_x_ref*t_ref
+    
+    return solve_ivp(_func, (t[0], t[-1]), y0, t_eval = t, args=(params, ), events=_turn_around)
--- S2/python/main.py
+++ S2/python/main.py
@@ -1,7 +1,41 @@
-from scipy.integrate import solve_ivp
-from scipy import constants as cst
 import numpy as np
 import matplotlib.pyplot as plt
 
+import kinetic_dimless as solver
+from atmosphere import Atmosphere
+
+atm = Atmosphere()
+
+def plot_x(sol, params):
+    plt.plot(sol.t / params.t_meco, sol.y[0] * params.x_ref / 100000.0)
+    plt.show()
+
+def plot_dot_x(sol, params):
+    Ma = []
+    
+    for i in range(len(sol.t)):
+        H = sol.y[0][i] * params.x_ref
+        Ma.append(sol.y[1][i] * params.c0 / atm.speed_of_sound(H))
+    
+    plt.plot(sol.t / params.t_meco, Ma)
+    plt.show()
+
 if __name__ == '__main__':
+    params = solver.Parameters()
+    
+    t = np.linspace(0, 10)
+    y0 = (0.0, 0.0)
+    
+    params.T = 2.0
+    params.Ma_ce = 8.16
+    params.Ma_L = 0.16
+    params.CD = 0.05
+    params.CA_L = 1.5
+    params.mf = 0.5
+    
+    sol = solver.solve_trajectory(t, y0, params)
+    
+    plot_dot_x(sol, params)
+    plot_x(sol, params)
+    
     print("done")
